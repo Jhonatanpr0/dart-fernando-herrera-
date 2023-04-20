052edfdaa296564ecdcb77469346958cc27e8e9e
Jhonatan A <jhonatanmavgs8@gmail.com>
2023-04-20 10:06:31 -0500

    abstract

--- 08_getters_setters.dart
+++ 08_getters_setters.dart
@@ -1,5 +1,5 @@
 void main() {
-  final mySquare = Square(side: 10.0);
+  final mySquare = Square(side: -10);
   mySquare.side = -5;
   print('Area del cuadrado: ${mySquare.area}');
 }
@@ -8,7 +8,8 @@ class Square{
   double _side;
   
   Square({required double side})
-    : _side = side;
+    :assert(side >=0, 'el lado debe ser >= 0'), //todo: Aserción (para dar una consdicion y si no cumple dar un mensaje de error)
+     _side = side;
   
   double get area {
     
@@ -17,7 +18,7 @@ class Square{
   
   set side(double value){
     print('Settings new value $value');
-    if(value < 0) throw 'Value most be >=0';
+    if(value < 0) throw 'Value most be >= 0';
     
     _side = value;
   }
